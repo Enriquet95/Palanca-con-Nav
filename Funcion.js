2ddf4a0b6979ecbcd11c0ed81ab317e195906a72
Enri <122067941+Enriquet95@users.noreply.github.com>
2024-02-20 07:48:25 -0500

    Add files via upload

---
+++ Funcion.js
@@ -0,0 +1,14 @@
+function abc(){
+    document.getElementById('activo').style.display="none";
+    document.getElementById('inactivo').style.display="block";
+    document.getElementById('body').style.background="whitesmoke";
+    document.getElementById('caja').style.background="#fff";
+    document.getElementById('h4').style.color="steelblue";
+}
+function bca(){
+    document.getElementById('inactivo').style.display="none";
+    document.getElementById('activo').style.display="block";
+    document.getElementById('body').style.background="#333";
+    document.getElementById('caja').style.background="#444";
+    document.getElementById('h4').style.color="#3fa";
+}
